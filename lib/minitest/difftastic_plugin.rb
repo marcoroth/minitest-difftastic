3bb4eb32c489875a1e7e4660ec95043f67d6e02b
Marco Roth <marco.roth@intergga.ch>
2025-01-22 17:35:03 +0100

    Remove custom labels code now that it's upstream

--- lib/minitest/difftastic_plugin.rb
+++ lib/minitest/difftastic_plugin.rb
@@ -5,44 +5,21 @@ require "difftastic"
 # TODO: use refinements
 module Minitest
   module Assertions
-    MINIMUM_OFFSET = 29
-    TAB_WIDTH = 2
-
-    RED = "\e[91;1m"
-    GREEN = "\e[92;1m"
-    RESET = "\e[0m"
-
     DIFFER = ::Difftastic::Differ.new(
       color: :always,
       tab_width: TAB_WIDTH,
-      syntax_highlight: :off
+      syntax_highlight: :off,
+      left_label: "Expected",
+      right_label: "Actual"
     )
 
     alias diff_original diff
 
     def diff(exp, act)
-      diff = DIFFER.diff_objects(exp, act)
-      offset = actual_text_offset(diff.split("\n").first)
-
-      "\n#{RED}#{"Expected".ljust(offset)} #{GREEN}Actual#{RESET}\n#{diff}"
+      DIFFER.diff_objects(exp, act)
     rescue StandardError => e
       puts "Minitest::DiffTastic error: #{e.inspect} (#{e.backtrace[0]})"
       diff_original(exp, act)
-    end
-
-    private
-
-    def strip_ansi_formatting(string)
-      string.to_s.gsub(/\e\[[0-9;]*m/, "")
-    end
-
-    def actual_text_offset(line)
-      stripped_line = strip_ansi_formatting(line)
-      _lhs, rhs = stripped_line.split(/\s{#{TAB_WIDTH},}/, 2)
-
-      offset = stripped_line.index("#{" " * TAB_WIDTH}#{rhs}") + TAB_WIDTH - 1
-
-      [MINIMUM_OFFSET, offset].max
     end
   end
 end
