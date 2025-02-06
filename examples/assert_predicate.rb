f1b14c850c3ee37f486de6ad35bfdd1fd64529b1
Marco Roth <marco.roth@intergga.ch>
2025-02-06 05:51:53 +0100

    Add assert_predicate examples

---
+++ examples/assert_predicate.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "example_helper"
+
+module Examples
+  class AssertPredicate < Minitest::Test
+    def test_assert_predicate_array_empty
+      assert_predicate [1], :empty?
+    end
+
+    def test_assert_predicate_string_empty
+      assert_predicate "1", :empty?
+    end
+
+    def test_assert_predicate_string_frozen
+      assert_predicate(+"", :frozen?)
+    end
+
+    def test_assert_predicate_with_custom_method
+      test = Data.define(:enabled) do
+        def self.name
+          "Config"
+        end
+
+        def enabled?
+          enabled
+        end
+      end
+
+      assert_predicate test.new(enabled: false), :enabled?
+    end
+  end
+end
