d6a4cf6990b58f88cc520d3695e83cbc5f8e36cc
Marco Roth <marco.roth@intergga.ch>
2025-02-06 02:33:57 +0100

    Wrap msg in the the message block

--- lib/minitest/difftastic/patches/assert_nil.rb
+++ lib/minitest/difftastic/patches/assert_nil.rb
@@ -4,15 +4,17 @@ module Minitest::Assertions
   alias minitest_assert_nil assert_nil
 
   def assert_nil(obj, msg = nil)
-    differ = ::Difftastic::Differ.new(
-      color: :always,
-      tab_width: 2,
-      syntax_highlight: :off,
-      left_label: "Expected",
-      right_label: "Actual"
-    )
+    msg ||= message(nil, "") {
+      differ = ::Difftastic::Differ.new(
+        color: :always,
+        tab_width: 2,
+        syntax_highlight: :off,
+        left_label: "Expected",
+        right_label: "Actual"
+      )
 
-    msg ||= differ.diff_objects(nil, obj)
+      "\n#{differ.diff_objects(nil, obj)}"
+    }
 
     minitest_assert_nil(obj, msg)
   rescue StandardError
