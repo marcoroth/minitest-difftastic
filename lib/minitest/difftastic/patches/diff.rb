0100b6d10b68b753fccaf1b7948b650f3539bdc2
Ufuk Kayserilioglu <ufuk@paralaus.com>
2025-02-06 14:03:18 -0500

    Change patches to use prepend instead of alias method (#13)

--- lib/minitest/difftastic/patches/diff.rb
+++ lib/minitest/difftastic/patches/diff.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
-module Minitest::Assertions
-  alias minitest_diff diff
+module Difftastic
+  module Patches
+    module Diff
+      def diff(exp, act)
+        case [exp, act]
+        in [String => exp, String => act] if exp.include?("\n") || act.include?("\n")
+          "\n#{::Minitest::Difftastic::STRING_DIFFER.diff_strings(exp, act)}"
+        else
+          "\n#{::Minitest::Difftastic::DEFAULT_DIFFER.diff_objects(exp, act)}"
+        end
+      rescue StandardError => e
+        puts "Minitest::Difftastic error: #{e.inspect} (#{e.backtrace[0]})"
+        super
+      end
 
-  def diff(exp, act)
-    case [exp, act]
-    in [String => exp, String => act] if exp.include?("\n") || act.include?("\n")
-      "\n#{::Minitest::Difftastic::STRING_DIFFER.diff_strings(exp, act)}"
-    else
-      "\n#{::Minitest::Difftastic::DEFAULT_DIFFER.diff_objects(exp, act)}"
+      Minitest::Assertions.prepend(self)
     end
-  rescue StandardError => e
-    puts "Minitest::Difftastic error: #{e.inspect} (#{e.backtrace[0]})"
-    minitest_diff(exp, act)
   end
 end
