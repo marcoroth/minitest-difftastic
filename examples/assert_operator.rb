8e261b0d4517ce806c9f58418c3a8cc5f8e803bd
Marco Roth <marco.roth@intergga.ch>
2025-02-06 03:01:45 +0100

    Add assert_operator examples

---
+++ examples/assert_operator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "example_helper"
+
+module Examples
+  class AssertOperator < Minitest::Test
+    def test_operator_case_equality
+      assert_operator "Left", :===, "Right"
+    end
+
+    def test_operator_eql
+      assert_operator 1, :eql?, 1.0
+    end
+
+    def test_operator_eql
+      assert_operator 1, :equal?, 1.0
+    end
+
+    def test_operator_lt
+      assert_operator 1, :>, 2
+    end
+  end
+end
